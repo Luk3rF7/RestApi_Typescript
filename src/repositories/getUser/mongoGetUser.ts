6cca11a4a19a82fd2cd53f465be43a00d9ce34fe
luk3rF7 <118012621+Luk3rF7@users.noreply.github.com>
2023-07-17 20:28:27 -0300

    feat:add id user

--- src/repositories/getUser/mongoGetUser.ts
+++ src/repositories/getUser/mongoGetUser.ts
@@ -1,25 +1,22 @@
 import { IGetUsersRepository } from '../../controller/getUsers/getUserProtocol';
+import { MongoClient } from '../../database/mongo';
 import { User } from '../../models/user';
 
 export class MongoGetUserRepository implements IGetUsersRepository {
  async  getUsers(): Promise<User[]> {
-    return [{
-      firstName:'usuario number 1',
-      lastName: 'testing',
-      email: 'user@example.com',
-      password:'123456'
-    }]
+  const user = await MongoClient
+  .db.collection<User>('users')  // pega todo usuario
+  .find({}) // vai buscar
+  .toArray() // converte em array
+
+  user[0].
+    return [{}]
   }
 }
 
 // * caso  eu queira adicionar outro banco de dados só criar seguinte repo:
 export class PostgressGetUserRepository implements IGetUsersRepository {
  async  getUsers(): Promise<User[]> {
-    return [{
-      firstName:'usuario number 1 do postgress',
-      lastName: 'testing2',
-      email: 'userPostgress@example.com',
-      password:'123456'
-    }]
+    return []
   }
 }
