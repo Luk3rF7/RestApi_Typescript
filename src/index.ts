a18197684455a877a5cd41c867a18d91cc7ab2f4
luk3rF7 <118012621+Luk3rF7@users.noreply.github.com>
2023-07-17 17:54:49 -0300

    chore:initial setup  express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world!")
+import express from 'express'
+
+// setup dotenv
+import { config } from 'dotenv'
+config()
+
+const app = express()
+
+const port = process.env.PORT || 8000;
+
+app.listen(port,() => {
+  console.log('iniciado Backend...')
+  console.log(`http://localhost:${port}`)
+})
